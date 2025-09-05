58a336c5d533a0a6c76ef26a528b375f9eafdcb3
Stephen Appah <appahstephen9@gmail.com>
2025-09-05 01:26:06 +0000

    imported ineject

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { HousingLocation } from "./housing-location";
 import { Observable, of } from "rxjs";
