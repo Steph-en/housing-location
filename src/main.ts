806b0d1932724f7e36fb595b4f296653c2492ff0
Stephen Appah <appahstephen9@gmail.com>
2025-09-05 01:21:47 +0000

    Added HttpClient to main.ts to render http requests

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routeConfig)
+    provideRouter(routeConfig),
+    provideHttpClient()
   ]
 }).catch(err => console.error(err));
